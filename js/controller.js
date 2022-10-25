d7ff12a16f1b28b1ade8d025aaa35b021ded97c4
barzaken <bar.zaken@boyar.org.il>
2022-10-25 18:51:23 +0300

    first try

---
+++ js/controller.js
@@ -0,0 +1,47 @@
+'use strict'
+
+
+let gElCanvas
+let gCtx
+let gCanvasTouch
+function init() {
+    gElCanvas = document.getElementById('my-canvas')
+    gCtx = gElCanvas.getContext('2d')
+    gCanvasTouch = new Hammer(gElCanvas)
+    resizeCanvas()
+    onTouch()
+    window.addEventListener('resize', resizeCanvas)
+}
+
+function downloadImg(elLink) {
+    const imgContent = gElCanvas.toDataURL('image/jpeg')// image/jpeg the default format
+    elLink.href = imgContent
+}
+
+function onTouch() {
+    gCanvasTouch.on('pan', (ev) => {
+        console.log(ev)
+        ev.preventDefault()
+        gIsDrawing = true
+        draw(null, ev.center.x, ev.center.y)
+    })
+}
+
+function resizeCanvas() {
+    const elContainer = document.querySelector('.canvas-container')
+    gElCanvas.width = elContainer.offsetWidth
+    gElCanvas.height = elContainer.offsetHeight
+}
+
+function onClearCanvas() {
+    gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height);
+}
+
+function onChangeColor(color) {
+    setColor(color)
+}
+
+function onChangeShape(shape) {
+    setShape(shape)
+}
+
---
+++ js/service.js
@@ -0,0 +1,74 @@
+'use strict'
+
+let gCurrShape = 'line'
+let gCurrColor
+let gIsDrawing = false
+
+function onStartDraw(ev){
+    gIsDrawing = true
+    gCtx.beginPath()
+    gCtx.moveTo(ev.offsetX, ev.offsetY)
+}
+
+function onEndDraw(){
+    gIsDrawing = false
+}
+
+function draw(ev,x,y) {
+    if(!gIsDrawing) return
+
+    const offsetX = ev.offsetX || x
+    const offsetY = ev.offsetY || y
+    switch (gCurrShape) {
+        case 'triangle':
+            drawTriangle(offsetX, offsetY)
+            break
+        case 'square':
+            drawSquare(offsetX, offsetY)
+            break
+        case 'circle':
+            drawCircle(offsetX, offsetY)
+            break
+        case 'line':
+            drawLine(offsetX, offsetY)
+            break
+    }
+}
+
+function drawLine(x, y, xEnd = 100, yEnd = 100) {
+    gCtx.lineWidth = 2
+    gCtx.lineTo(x, y)
+    gCtx.strokeStyle = `${gCurrColor}`
+    gCtx.stroke()
+}
+
+function drawTriangle(x, y) {
+    gCtx.lineWidth = 2
+    gCtx.moveTo(x, y)
+    gCtx.lineTo(x, y+200)
+    gCtx.lineTo(x+200, y)
+    gCtx.closePath()
+
+    gCtx.strokeStyle = `${gCurrColor}`
+    gCtx.stroke()
+}
+
+function drawSquare(x, y) {
+    gCtx.strokeStyle = `${gCurrColor}`
+    gCtx.strokeRect(x, y, 150, 300)
+}
+
+function drawCircle(x, y) {
+    gCtx.lineWidth = 6
+    gCtx.arc(x, y, 100, 0, 2 * Math.PI) // Used to create a circle 
+    gCtx.strokeStyle = `${gCurrColor}`
+    gCtx.stroke()
+  }
+
+function setShape(shape) {
+    gCurrShape = shape
+}
+
+function setColor(color) {
+    gCurrColor = color
+}
